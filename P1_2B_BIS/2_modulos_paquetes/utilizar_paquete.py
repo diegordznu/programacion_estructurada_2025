ea3f942dfe887ab842cbeaa50868163c26c84348
diegordznu <diego_3041240393@utd.edu.mx>
2025-07-21 08:53:32 -0600

    Delete P1_2B_BIS directory

--- P1_2B_BIS/2_modulos_paquetes/utilizar_paquete.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Diego Rodriguez"))
-
-modulos.borrarPantalla()
-nombre,tel=modulos.solicitarDatos2()
-print(f"\n..:Agenda Telefonica:..\n\t\tNombre: {nombre}\n\t\t Telefono: {tel}")
-modulos.esperarTecla()
--- P1_2B_BIS/6_tuple/tuple.py
+++
@@ -1,6 +0,0 @@
-"""
-las tuplas se utilizan para almacenar varios elementos en una sola variable
-
-una tupla es una coleccion ordenada e inmutable
-
-las tuplas se escribem entre parentesis"""
